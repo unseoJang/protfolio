790a30183ccfa3715977e7ef40ccdccfdb94316f
이모은(Moeun Lee) <2m2@KOREA.NCSOFT.CORP>
2016-11-01 16:13:04 +0900

    스크롤 모션 1차 개발

---
+++ portfolio_test/js/project_data.js
@@ -0,0 +1,55 @@
+/*****************************************
+	프로젝트 데이터 모음
+	* 해당 사항이 없을 때, 빈칸으로 남겨둡니다.
+
+	- title			: 프로젝트 이름
+	- date			: 프로젝트 일정, 0000.00 - 0000.00
+	- members		: 작업 총 인원
+	- participate	: 참여비율, 0~100 (plan/design/dev/markup/style/javascript/php..)
+	- client		: 클라이언트
+	- type			: 프로젝트 타입 (web/app/video/...)
+	- summary		: 프로젝트 요약 설명
+	- explanation	: 프로젝트 상세 설명
+	- description	: 느낀점/작업하며어려웠던점/새롭게해낸점/기억에남는점 등 자유롭게 기술
+*****************************************/
+
+var $project = [
+	{
+		title:'',
+		date:'',
+		members:'',
+		participate:{
+			plan:'',
+			design:'',
+			dev:'',
+			markup:'',
+			style:'',
+			javascript:'',
+			php:''
+		},
+		client:'',
+		type:'',
+		summary:'',
+		explanation:'',
+		description:''
+	},
+	{
+		title:'',
+		date:'',
+		members:'',
+		participate:{
+			plan:'',
+			design:'',
+			dev:'',
+			markup:'',
+			style:'',
+			javascript:'',
+			php:''
+		},
+		client:'',
+		type:'',
+		summary:'',
+		explanation:'',
+		description:''
+	}
+];
